4d434ba116a1b25ce7ebfe002a9aabda2680088c
Shelly Chen <shellyjychen@gmail.com>
2018-08-05 05:27:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,24 +3,12 @@ class Dog
     @name = name
   end
 
-  def name
-    "#{name}".strip
-  end
-
   def name=(name)
     @name = (name)
   end
 
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed
-    "{breed}".strip
-  end
-
-  def breed=(new_breed)
-    @breed = (new_breed)
+  def name
+    "#{name}".strip
   end
 
 end
